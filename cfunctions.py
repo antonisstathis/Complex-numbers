193d3eae1e1f8b9ffd0252f2b0a74286aff250cb
antonisstathis <antonisstathis95@gmail.com>
2021-01-31 17:33:23 +0200

    Update cfunctions.py

--- cfunctions.py
+++ cfunctions.py
@@ -45,29 +45,31 @@ def div(complex1,complex2,complex3):
 
 def zparallel(complex1,complex2,complex3):
 
-    real1=complex1["real"]
-    im1=complex1["im"]
-    real2=complex2["real"]
-    im2=complex2["im"]
-    real=real1*real2+(-1)*im1*im2
-    im=(real1*im2+im1*real2)
-    print("real part="+str(real)+" imaginary part="+str(im))
+    real1 = complex1["real"]
+    im1 = complex1["im"]
+    real2 = complex2["real"]
+    im2 = complex2["im"]
+    real = real1 * real2 + (-1) * im1 * im2
+    im = (real1 * im2 + im1 * real2)
+    print("real part=" + str(real) + " imaginary part=" + str(im))
 
-    if (input("Enter 'c' to continue:"))=='c':
-        real3=real1+real2
-        im3=im1+im2
-        print("real part="+str(real3)+" imaginary part="+str(im3))
-            
-        real4=real*real3+(-1)*im*((-1)*im3)
-        im4=(real*(-1)*im3+im*real3)
-        print("real part="+str(real4)+" imaginary part="+str(im4))
+    if (input("Enter 'c' to continue:")) == 'c' and (real1!=0 or im1!=0) and (real2!=0 or im2!=0):
+        real3 = real1 + real2
+        im3 = im1 + im2
+        print("real part=" + str(real3) + " imaginary part=" + str(im3))
 
-        result=real3*real3+im3*im3
-        print("result="+str(result))
-            
-        real5=real4/result
-        im5=im4/result
-        print("real part="+str(real5)+" imaginary part="+str(im5))
-        complex3["real"]=real5
-        complex3["im"]=im5
+        real4 = real * real3 + (-1) * im * ((-1) * im3)
+        im4 = (real * (-1) * im3 + im * real3)
+        print("real part=" + str(real4) + " imaginary part=" + str(im4))
+
+        result = real3 * real3 + im3 * im3
+        print("result=" + str(result))
+
+        real5 = real4 / result
+        im5 = im4 / result
+        print("real part=" + str(real5) + " imaginary part=" + str(im5))
+        complex3["real"] = real5
+        complex3["im"] = im5
         print("Final result:")
+    else:
+        print("zp function impossible: you entered zero complex number.")
