1988e35d4ddb2c4263b0d3d9b0462d17443ca962
antonisstathis <antonisstathis95@gmail.com>
2021-01-31 15:53:24 +0200

    Add files via upload

---
+++ c.py
@@ -0,0 +1,54 @@
+from trans import enterp,enterc,transp,transc
+from cproduct import add,mul,div,zparallel
+
+complex1={
+  "real": 0.0,
+  "im": 0.0,
+  "measure": 0.0,
+  "corner": 0.0
+  }
+
+complex2={
+  "real": 0.0,
+  "im": 0.0,
+  "measure": 0.0,
+  "corner": 0.0
+}
+
+complex3={
+  "real": 0.0,
+  "im": 0.0,
+  "measure": 0.0,
+  "corner": 0.0
+}
+
+com=[complex1,complex2]
+
+x="go"
+while x=="go":
+    for i in range(2):
+        choice=input("Enter form:")    
+        if choice=='p':
+            enterp(com[i])
+        if choice=='c':
+            enterc(com[i])
+
+    print("Enter: 'add' or 'mul' or 'div' or 'zp'")
+    choice=input("Enter function:")
+    if choice=="add":
+        add(complex1,complex2,complex3)
+        transp(complex3)
+    if choice=="mul":
+        mul(complex1,complex2,complex3)
+        transc(complex3)
+    if choice=="div":
+        div(complex1,complex2,complex3)
+        transc(complex3)
+    if choice=="zp":
+        print(complex1["real"])
+        print(complex2["real"])
+        zparallel(complex1,complex2,complex3)
+        transp(complex3)
+
+    x=input("Enter 'go' or 'exit':")
+    print("\n\n")
---
+++ trans.py
@@ -0,0 +1,64 @@
+import math
+
+def transc(complex1):
+    m=complex1["measure"]
+    c=complex1["corner"]
+    corner=math.radians(c)
+    if c==0:
+        real=m
+        im=0
+    if c==180:
+        real=(-1)*m
+        im=0
+    if c==90:
+        real=0
+        im=m*math.sin(corner)
+    if c==270:
+        real=0
+        im=m*math.sin(corner)
+    if c!=0 and c!=180 and c!=90 and c!=270:
+        real=m*math.cos(corner)
+        im=m*math.sin(corner)
+    print("Real="+str(real)+" Imaginary="+str(im))
+    print("Measure="+str(m)+" Corner="+str(c))
+    complex1["real"]=real
+    complex1["im"]=im
+
+def transp(complex1):
+    x=complex1["real"]
+    y=complex1["im"]
+    d=math.sqrt(x*x+y*y)
+    if  x>0 and y>0:
+        c=math.degrees(math.atan(y/x))
+    if x<0 and y>0:
+        c=180+math.degrees(math.atan(y/x))
+    if x<0 and y<0:
+        c=180+math.degrees(math.atan(y/x))
+    if x>0 and y<0:
+        c=360+math.degrees(math.atan(y/x))
+    if x==0 and y>0:
+        c=90
+    if x==0 and y<0:
+        c=270
+    if x>0 and y==0:
+        c=0
+    if x<0 and y==0:
+        c=180
+    print("Real="+str(x)+" Imaginary="+str(y))
+    print("Measure="+str(d)+" Corner="+str(c))
+    complex1["measure"]=d
+    complex1["corner"]=c
+
+def enterp(complex1):
+    m=float(input("Enter measure: "))
+    c=float(input("Enter corner: "))
+    complex1["measure"]=m
+    complex1["corner"]=c
+    transc(complex1)
+
+def enterc(complex1):
+    x=float(input("Enter real part: "))
+    y=float(input("Enter imaginary part: "))
+    complex1["real"]=x
+    complex1["im"]=y
+    transp(complex1)
